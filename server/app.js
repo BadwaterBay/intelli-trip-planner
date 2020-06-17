e2ff78fcae71c66471eb10793a0f3ff03b49d142
Skyler Dong <dongskyler@gmail.com>
2020-06-16 23:13:18 -0500

    Add Babel and configs to server

--- server/app.js
+++ server/app.js
@@ -1,6 +1,45 @@
 import express from 'express';
+import createError from 'http-errors';
+import path from 'path';
+import cors from 'cors';
+import logger from 'morgan';
+import cookieParser from 'cookie-parser';
+import bodyParser from 'body-parser';
+// import favicon from 'serve-favicon';
+import indexRouter from './routes/index';
 
 // Create Express server
 const app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(cors());
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+// app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(bodyParser.json());
+
+app.use('/', indexRouter);
+
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+  next(createError(404));
+});
+
+// error handler
+app.use((err, req, res, next) => {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
 
 export default app;
