02d600e57a85b8ebb3059a0a9e28405b9316c2f1
Skyler Dong <dongskyler@gmail.com>
2020-07-19 19:57:40 -0500

    Create distance-duration matrices & solve for first route optimization

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,8 @@
 module.exports = {
-  env: {
-    browser: true,
-    commonjs: true,
-    node: true,
-    es2020: true,
-  },
+  env: { browser: true, commonjs: true, node: true, es2020: true },
   extends: ['airbnb-base', 'plugin:prettier/recommended'],
-  globals: {
-    Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly',
-  },
-  parserOptions: {
-    ecmaVersion: 2020,
-    sourceType: 'module',
-  },
+  globals: { Atomics: 'readonly', SharedArrayBuffer: 'readonly' },
+  parserOptions: { ecmaVersion: 2020, sourceType: 'module' },
   rules: {
     'new-cap': 1,
     'no-invalid-this': 1,
--- src/lib/get_distance_matrix.py
+++ src/lib/get_distance_matrix.py
@@ -1,39 +1,78 @@
 #!/usr/bin/env python
 
-"""
-Get Distance Matrix from Google Maps
-"""
-
 import os
-import json
 from dotenv import load_dotenv
 import googlemaps
+import json
+import pickle
 
-load_dotenv()
-API_KEY = os.environ.get("API_KEY_GOOGLE")
 
-gmaps = googlemaps.Client(key=API_KEY)
+def get_distance_matrix(origins: []) -> dict:
+    """
+    Get distance matrix from Google Maps API
+    """
 
-origins = [
-    "Las Vegas McCarran International Airport, NV",
-    "Los Angeles International Airport",
-    "Death Valley Furnace Creek Visitor Center, Furnace Creek, CA",
-    "Mojave Kelso Depot Visitor Center, CA",
-    "Joshua Tree National Park Visitor Center, Park Boulevard, Joshua Tree, California",
-    "Sequoia National Park - Visitor Center, Generals Highway, Three Rivers, CA",
-    "Zion National Park Visitor Center, Zion – Mount Carmel Highway, Hurricane, UT",
-    "Bryce Canyon National Park Visitor Center, Utah 63, Bryce Canyon City, UT",
-    "Grand Canyon North Rim Visitor Center, AZ-67, North Rim, AZ 86023",
-    "Grand Canyon Visitor Center, South Entrance Road, Grand Canyon Village, AZ",
-]
+    load_dotenv()
+    API_KEY = os.environ.get("API_KEY_GOOGLE")
 
-destinations = origins
+    gmaps = googlemaps.Client(key=API_KEY)
 
-# Get distance matrix
-distance_matrix = gmaps.distance_matrix(origins, destinations)
+    destinations = origins
 
-# print(distance_matrix)
+    # Get distance matrix
+    distance_matrix = gmaps.distance_matrix(origins, destinations)
 
-# Save distance matrix to file
-with open("./src/data/distance_matrix.json", "w") as file:
-    json.dump(distance_matrix, file, indent=2)
+    return distance_matrix
+
+
+def save_distance_matrix_to_json(distance_matrix: dict, to_file: str) -> None:
+    """
+    Save distance matrix to json file
+    """
+
+    with open(to_file, "w") as file:
+        json.dump(distance_matrix, file, indent=2)
+
+
+def convert_json_to_list(json_file: str, type: str) -> [[int]]:
+    """
+    Convert distance matrix JSON to 2D list for OR-Tools
+
+    "type" can be either "distance" or "duration"
+    """
+
+    with open(json_file, "r") as read_file:
+        data = json.load(read_file)
+
+    len_1d: int = len(data["rows"])
+    len_2d: int = len(data["rows"][0]["elements"])
+
+    distance_matrix_list: [] = [None] * len_1d
+
+    for i in range(0, len_1d):
+        inner_list: [] = [None] * len_2d
+
+        for j in range(0, len_2d):
+            inner_list[j] = data["rows"][i]["elements"][j][type]["value"]
+
+        distance_matrix_list[i] = inner_list
+
+    return distance_matrix_list
+
+
+def save_distance_matrix_list_to_txt(distance_list: [[int]], to_file: str) -> None:
+    """
+    Save distance list to txt file
+    """
+
+    with open(to_file, "w") as file:
+        file.write("%s\n" % distance_list)
+
+
+def pickle(data, to_file: str) -> None:
+    """
+    Save distance list to pickle file
+    """
+
+    with open(to_file, "wb") as file:
+        pickle.dump(data, file)
