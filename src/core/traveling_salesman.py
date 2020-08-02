b6d71285a122a75c286d47bf2530aa245c557547
Skyler Dong <dongskyler@gmail.com>
2020-08-02 01:16:56 -0500

    :ci: Add unit testing

--- src/core/traveling_salesman.py
+++ src/core/traveling_salesman.py
@@ -5,14 +5,15 @@ Solve for the fastest route
 """
 
 from __future__ import print_function
+import os
 import pickle
 from ortools.constraint_solver import routing_enums_pb2
 from ortools.constraint_solver import pywrapcp
 
 
-def create_data_model():
+def create_data_model(filepath):
     """Stores the data for the problem."""
-    with open("./src/data/distance_matrix_list.pkl", "rb") as file:
+    with open(filepath, "rb") as file:
         distance_matrix = pickle.load(file)
     data = {}
     data["distance_matrix"] = distance_matrix
@@ -21,26 +22,47 @@ def create_data_model():
     return data
 
 
-def print_solution(manager, routing, solution):
+def get_solution(manager, routing, solution):
     """Prints solution on console."""
-    print("Objective: {} miles".format(solution.ObjectiveValue()))
     index = routing.Start(0)
-    plan_output = "Route for vehicle 0:\n"
-    route_distance = 0
+    plan = {
+        "waypoints": [],
+        "objective_value": solution.ObjectiveValue(),
+        "true_value": 0,
+    }
     while not routing.IsEnd(index):
-        plan_output += " {} ->".format(manager.IndexToNode(index))
+        plan["waypoints"].append(manager.IndexToNode(index))
         previous_index = index
         index = solution.Value(routing.NextVar(index))
-        route_distance += routing.GetArcCostForVehicle(previous_index, index, 0)
-    plan_output += " {}\n".format(manager.IndexToNode(index))
+        plan["true_value"] += routing.GetArcCostForVehicle(previous_index, index, 0)
+    plan["waypoints"].append(manager.IndexToNode(index))
+    return plan
+
+
+def print_plan(plan) -> None:
+    """Prints solution on console."""
+    if plan is None:
+        print("No solution found.")
+        return
+    plan_output = ""
+    length = len(plan["waypoints"])
+    for i in range(length):
+        if i > 0:
+            plan_output += " "
+        plan_output += "{}".format(plan["waypoints"][i])
+        if i < length - 1:
+            plan_output += " ->"
+    print("Objective: {} meters".format(plan["objective_value"]))
+    print("True: {} meters".format(plan["true_value"]))
     print(plan_output)
-    plan_output += "Route distance: {}miles\n".format(route_distance)
+    return
 
 
-def main():
+def solve_for_plan(filepath):
     """Entry point of the program."""
+
     # Instantiate the data problem.
-    data = create_data_model()
+    data = create_data_model(filepath)
 
     # Create the routing index manager.
     manager = pywrapcp.RoutingIndexManager(
@@ -64,16 +86,29 @@ def main():
 
     # Setting first solution heuristic.
     search_parameters = pywrapcp.DefaultRoutingSearchParameters()
+    # pylint: disable=no-member
     search_parameters.first_solution_strategy = (
         routing_enums_pb2.FirstSolutionStrategy.PATH_CHEAPEST_ARC
     )
+    # pylint: enable=no-member
 
     # Solve the problem.
     solution = routing.SolveWithParameters(search_parameters)
 
     # Print solution on console.
     if solution:
-        print_solution(manager, routing, solution)
+        plan = get_solution(manager, routing, solution)
+        return plan
+
+
+def main():
+    """
+    Driver function
+    """
+    cur_dir = os.path.dirname(os.path.abspath(__file__))
+    filepath = os.path.join(cur_dir, "data/distance_matrix_list.pkl")
+    plan = solve_for_plan(filepath)
+    print_plan(plan)
 
 
 if __name__ == "__main__":
