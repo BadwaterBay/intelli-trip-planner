9371789737b080b3622d03983317733a6cb0cf69
Skyler Dong <dongskyler@gmail.com>
2020-08-05 03:32:11 -0500

    :star: Catch corner & error cases & add default values to arguments

--- src/core/helpers/helper.py
+++ src/core/helpers/helper.py
@@ -1,12 +1,25 @@
 #!/usr/bin/env python
 
 """
-Uncategorized helper functions
+Helper functions
 """
 
+import sys
+from typing import Union
 
-def seconds_in_x_hours(x_hours: int) -> int:
+
+def seconds_in_x_hours(x_hours: Union[int, float, None] = 0) -> int:
     """
-    Calculate the number of seconds in x hours
+    # Calculate the number of seconds in x hours
+    - :param x_hours: Nonnegative integer representing the number of hours.
+    - :type x_hours: int | float | None
+    - :return: the number of seconds in x hours. Default to 0 if error occurs.
+    - :rtype: int
     """
-    return int(3600 * x_hours)
+    if isinstance(x_hours, (int, float)) and (x_hours >= 0):
+        return round(3600 * x_hours)
+    print(
+        "A negative x_hours or an invalid data type was given. Return default value 0.",
+        file=sys.stderr,
+    )
+    return 0
